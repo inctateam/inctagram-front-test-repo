d7ef8b19d1a8bd0f41425f0dcf024acbcc4acb4b
apastan <apastan.dev@gmail.com>
2024-09-21 18:53:59 +0300

    feat: test 19

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         />
         <ol
           className={
-            'list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]'
+            'hi list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]'
           }
         >
           <li className={'mb-2'}>
